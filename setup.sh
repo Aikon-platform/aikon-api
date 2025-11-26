4104bf6842c92079b44300849b25139b58b8989c
Segolene-Albouy <segolene.albouy@gmail.com>
2025-11-26 12:18:57 +0100

    [WIP] simpler setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 INSTALL_MODE=${INSTALL_MODE:-"full_install"}
 source "$SCRIPT_DIR"/docker/utils.sh
-source "../front"
+source "../front/app/config/.env"
 
 color_echo cyan "Running a $INSTALL_MODE for the API! 🚀"
 
@@ -26,6 +26,7 @@ if [ "$answer" = "yes" ]; then
     color_echo yellow "\nSystem packages..."
     if [ "$OS" = "Linux" ]; then
         sudo apt-get install redis-server python3.10 python3.10-venv python3.10-dev curl
+        sudo systemctl restart redis-server
     elif [ "$OS" = "Mac" ]; then
         brew install redis python@3.10 curl
         brew services start redis
@@ -56,29 +57,6 @@ if [ "$TARGET" == "dev" ]; then
     color_echo yellow "\nPre-commit install"
     venv/bin/pip install pre-commit
     pre-commit install
-fi
-
-set_redis() {
-    redis_psw="$1"
-    REDIS_CONF=$(redis-cli INFO | grep config_file | awk -F: '{print $2}' | tr -d '[:space:]')
-    color_echo yellow "\nModifying Redis configuration file $REDIS_CONF ..."
-
-    # use the same redis password for api and front
-    sudo_sed_repl_inplace "s~^REDIS_PASSWORD=.*~REDIS_PASSWORD=\"$redis_psw\"~" "../front/app/config/.env"
-
-    sudo_sed_repl_inplace "s/\nrequirepass [^ ]*/requirepass $redis_psw/" "$REDIS_CONF"
-    sudo_sed_repl_inplace "s/# requirepass [^ ]*/requirepass $redis_psw/" "$REDIS_CONF"
-
-    if [ "$OS" = "Linux" ]; then
-        sudo systemctl restart redis-server
-    elif [ "$OS" = "Mac" ]; then
-        brew services restart redis
-    fi
-}
-color_echo blue "\nDo you want to add a password to redis?"
-answer=$(printf "%s\n" "${options[@]}" | fzy)
-if [ "$answer" = "yes" ]; then
-    set_redis $REDIS_PASSWORD
 fi
 
 color_echo blue "\nDo you want to init submodules?"
