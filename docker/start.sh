c2d821cab8f1fd3e32c2db488e079a1fa26515f6
Segolene-Albouy <segolene.albouy@gmail.com>
2026-07-07 15:48:06 +0200

    [SETUP] fix misc things local install

--- docker/start.sh
+++ docker/start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# API Docker
+# API Docker entrypoint
 
 set -e
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$ROOT_DIR"/utils.sh
 source "$ROOT_DIR"/api/.env
 
-# Create necessary directories at startup
-mkdir -p "$ROOT_DIR"/var/dramatiq/
-mkdir -p "$ROOT_DIR"/.config/matplotlib
-chown -R $USER "$ROOT_DIR"/.config/matplotlib
+color_echo() {
+    case "$1" in
+        green)  echo -e "\033[1;92m$2\033[0m";;
+        red)    echo -e "\033[1;91m$2\033[0m";;
+        blue)   echo -e "\033[1;94m$2\033[0m";;
+        *)      echo "$2";;
+    esac
+}
 
-source "$ROOT_DIR"/venv/bin/activate
+mkdir -p "$ROOT_DIR"/var/dramatiq/ "$ROOT_DIR"/.config/matplotlib
+chown -R "$USER" "$ROOT_DIR"/.config/matplotlib
+
+source /home/aikonapi/.venv/bin/activate
 
 is_build=0
 
@@ -26,7 +32,6 @@ if [[ "$INSTALLED_APPS" == *"vectorization"* ]]; then
     # pip install -e synthetic/
     is_build=1
 fi
-
 
 # if region in INSTALLED_APPS, and build is not already done
 if [[ "$INSTALLED_APPS" == *"region_extraction"* ]] && [[ $is_build -eq 0 ]]; then
